1f8a9c2496a6519ba2eec995a55f927f4f34ce73
Gabriele Gullì <magapano89@gmail.com>
2026-07-23 16:19:21 +0200

    feat: Configure Swagger to use JWT Bearer Authentication
    - Added SecurityDefinition and SecurityRequirement for Swagger UI to support Bearer tokens

--- KinematicsSimulator.API/Program.cs
+++ KinematicsSimulator.API/Program.cs
@@ -1,12 +1,32 @@
 using KinematicsSimulator.Application;
 using KinematicsSimulator.Infrastructure;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insert token in the text box"
+    });
+
+    options.AddSecurityRequirement(_ => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("Bearer"),
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
