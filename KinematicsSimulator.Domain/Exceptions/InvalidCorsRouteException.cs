f3e0580fdb6bb11819c5886c86ba0aafeabe0668
Gabriele Gullì <magapano89@gmail.com>
2026-07-25 10:16:58 +0200

    refactor(exceptions): use base constructor for status code and title

--- KinematicsSimulator.Domain/Exceptions/InvalidCorsRouteException.cs
+++ KinematicsSimulator.Domain/Exceptions/InvalidCorsRouteException.cs
@@ -1,24 +1,18 @@
 namespace KinematicsSimulator.Domain.Exceptions;
 
+#pragma warning disable RCS1194
 public class InvalidCorsRouteException : DomainException
 {
 
-    public InvalidCorsRouteException() : base("One or more allowed front-end CORS routes are missing from the configuration.")
+    public InvalidCorsRouteException() : base(
+        "One or more allowed front-end CORS routes are missing from the configuration.",
+        500,
+        "Cors configuration missing")
     {
-        StatusCode = 500;
-        Title = "Cors configuration missing";
     }
 
-    public InvalidCorsRouteException(string? message) : base(message)
+    public InvalidCorsRouteException(string message) : base(message, 500, "Cors configuration missing")
     {
-        StatusCode = 500;
-        Title = "Cors configuration missing";
-    }
-
-    public InvalidCorsRouteException(string? message, Exception? innerException) : base(message, innerException)
-    {
-        StatusCode = 500;
-        Title = "Cors configuration missing";
     }
 
 }
--- KinematicsSimulator.Domain/Exceptions/InvalidResultPatternException.cs
+++ KinematicsSimulator.Domain/Exceptions/InvalidResultPatternException.cs
@@ -1,23 +1,17 @@
 namespace KinematicsSimulator.Domain.Exceptions;
 
+#pragma warning disable RCS1194
 public class InvalidResultPatternException : DomainException
 {
-    public InvalidResultPatternException() : base("Invalid Result state: a successful result cannot contain errors, and a failed result must contain at least one error.")
+    public InvalidResultPatternException() : base(
+        "Invalid Result state: a successful result cannot contain errors, and a failed result must contain at least one error.",
+        500,
+        "Result pattern value error")
     {
-        StatusCode = 500;
-        Title = "Result pattern value error";
     }
 
-    public InvalidResultPatternException(string? message) : base(message)
+    public InvalidResultPatternException(string message) : base(message, 500, "Result pattern value error")
     {
-        StatusCode = 500;
-        Title = "Result pattern value error";
-    }
-
-    public InvalidResultPatternException(string? message, Exception? innerException) : base(message, innerException)
-    {
-        StatusCode = 500;
-        Title = "Result pattern value error";
     }
 
 }
