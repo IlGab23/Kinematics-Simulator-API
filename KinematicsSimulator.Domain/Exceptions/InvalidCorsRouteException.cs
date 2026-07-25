28a63fdc48c9f0cd092eefc16c0b33382c59a103
Gabriele Gullì <magapano89@gmail.com>
2026-07-25 09:54:36 +0200

    feat: Properties to exceptions for status code and title

--- KinematicsSimulator.Domain/Exceptions/InvalidCorsRouteException.cs
+++ KinematicsSimulator.Domain/Exceptions/InvalidCorsRouteException.cs
@@ -2,16 +2,25 @@ namespace KinematicsSimulator.Domain.Exceptions;
 
 public class InvalidCorsRouteException : Exception
 {
+    public int StatusCode { get; }
+    public string Title { get; }
+
     public InvalidCorsRouteException() : base("One or more allowed front-end CORS routes are missing from the configuration.")
     {
+        StatusCode = 500;
+        Title = "Cors configuration missing";
     }
 
     public InvalidCorsRouteException(string? message) : base(message)
     {
+        StatusCode = 500;
+        Title = "Cors configuration missing";
     }
 
     public InvalidCorsRouteException(string? message, Exception? innerException) : base(message, innerException)
     {
+        StatusCode = 500;
+        Title = "Cors configuration missing";
     }
 
 }
--- KinematicsSimulator.Domain/Exceptions/InvalidResultPatternException.cs
+++ KinematicsSimulator.Domain/Exceptions/InvalidResultPatternException.cs
@@ -2,14 +2,24 @@ namespace KinematicsSimulator.Domain.Exceptions;
 
 public class InvalidResultPatternException : Exception
 {
-    public InvalidResultPatternException() : base("Invalid Result state: a successful result cannot contain errors, and a failed result must contain at least one error.") {}
+    public int StatusCode { get; }
+    public string Title { get; }
+    public InvalidResultPatternException() : base("Invalid Result state: a successful result cannot contain errors, and a failed result must contain at least one error.")
+    {
+        StatusCode = 500;
+        Title = "Result pattern value error";
+    }
 
     public InvalidResultPatternException(string? message) : base(message)
     {
+        StatusCode = 500;
+        Title = "Result pattern value error";
     }
 
     public InvalidResultPatternException(string? message, Exception? innerException) : base(message, innerException)
     {
+        StatusCode = 500;
+        Title = "Result pattern value error";
     }
 
 }
